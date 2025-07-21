3e9f3e283a9e1add72dbf3715f0431c4868e7f86
unknown <shriyar04@gmail.com>
2025-07-21 18:11:56 +0530

    Initial commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,25 @@
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+function Favorites() {
+  const { favorites } = useMovieContext();
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="favorites-empty">
+      <h2>No favs</h2>
+      <p>start adding movies to your favs and they will appear here</p>
+    </div>
+  );
+}
+export default Favorites;
